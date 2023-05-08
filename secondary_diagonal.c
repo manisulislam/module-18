c95b3db88638b2cf2786eb1e570f7a9d1c438ded
anisul islam <anisulislam980340@gmail.com>
2023-05-08 10:34:45 +0600

    secondary diagonal matrix completed

---
+++ secondary_diagonal.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+  int row, col;
+  scanf("%d %d", &row , &col);
+  int a[row][col];
+  for(int i=0; i<row; i++){
+    for(int j=0; j<col; j++){
+        scanf("%d", &a[i][j]);
+    }
+  }
+
+  int flag=1;
+  if(row!=col){
+    printf("not diagonal matrix\n");
+  }
+
+  for(int i=0; i<row; i++){
+    for(int j=0; j<col; j++){
+        if(i+j==row-1){
+            continue;
+        }
+        if(a[i][j]!=0){
+            flag=0;
+        }
+    }
+  }
+  if(flag==1){
+    printf("secondary diagonal\n");
+  }
+  else{
+    printf("Not diagonal\n");
+  }
+  return 0;
+}
